af0892a10d18c1ae7e590a19600c3b6da17d89cb
Eileen Reid <eileenreid4657@gmail.com>
2020-01-20 10:47:50 -0500

    A tiny python program

---
+++ index.py
@@ -0,0 +1,10 @@
+def say_age(name, age):
+
+  print('hi {}. you are {} years old.'.format(name, age))
+say_age('eileen', '40')
+say_age('mark', '30')
+say_age('peggy', '24')
+say_age('Gen', '20')
+
+
+
